e5366a3cb05bb9ff98b282a3bd7ab3b204292639
xiaofeng.yao <871213171@qq.com>
2017-09-06 10:07:29 +0800

    2017.0906 update

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -7,17 +7,20 @@
  *   1、
  *   2、
  */
-/**
- * 通用：tab页面切换(tab在同级)
- */
-$('.nav-tab').click(function () {
-    $(this).parent().find(".active").removeClass();
-    $(this).addClass("active");
-})
-/**
- * 通用：tab页面切换(tab在上上级)
- */
-$('.nav-tab-child').click(function () {
-    $(this).parent().parent().find("#active").attr("id","");
-    $(this).attr("id","active");
+$(function(){
+    /**
+     * 通用：tab页面切换(tab在同级)
+     */
+    $('.nav-tab').click(function () {
+        $(this).parent().find(".active").removeClass();
+        $(this).addClass("active");
+    })
+    /**
+     * 通用：tab页面切换(tab在上上级)
+     */
+    $('.nav-tab-child').click(function () {
+        $(this).parent().parent().find("#active").attr("id","");
+        $(this).attr("id","active");
+    })
+
 })
