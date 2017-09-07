39ef2df67d3cdc4554dbc17f7b5f2da0e6425ea0
xiaofeng.yao <871213171@qq.com>
2017-09-07 13:41:17 +0800

    重新编写tab组件

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -7,7 +7,27 @@
  *   1、
  *   2、
  */
-$(function(){
+$(function () {
+    /**
+     * 通用url解析(获取到url上的值)
+     * @returns {Object}
+     * 如何调用？
+     * var request = getRequest(),
+     * total = request['total'];
+     */
+    function getRequest() {
+        var url = location.search;
+        var theRequest = new Object();
+        if (url.indexOf("?") != -1) {
+            var str = url.substr(1),
+                strs = str.split("&");
+            for (var i = 0; i < strs.length; i++) {
+                theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+            }
+        }
+        return theRequest;
+    }
+
     /**
      * 通用：tab页面切换(tab在同级)
      * active为灰色背景
@@ -16,33 +36,68 @@ $(function(){
         $(this).parent().find('.active').removeClass();
         $(this).addClass('active');
     });
+
     /**
      * 通用：tab页面切换(tab在上上级)
      * active为白色背景
      */
     $('.nav-tab-child').click(function () {
-        $(this).parent().parent().find('#active').attr('id','');
-        $(this).attr('id','active');
+        $(this).parent().parent().find('#active').attr('id', '');
+        $(this).attr('id', 'active');
     });
 
-});
+    /*
+    *   tab
+    *  
+        <div class="webTab">
+            <a name="tab1" class="nav-tab-span">tab1</a>
+            <a name="tab2" class="nav-tab-span">tab2</a>
+        </div>
+        <div>
+            <div id="tab1"></div>
+            <div id="tab2"></div>
+        </div>
+    *
+    */
+    function changeTabComponent() {
+        var $tabList = $('.webTab');
 
-/**
- * 通用url解析(获取到url上的值)
- * @returns {Object}
- * 如何调用？
- * var request = getRequest(),
- * total = request['total'];
- */
-function getRequest() {
-    var url = location.search;
-    var theRequest = new Object();
-    if (url.indexOf("?") != -1) {
-        var str = url.substr(1),
-            strs = str.split("&");
-        for (var i = 0; i < strs.length; i++) {
-            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+        function changeTab(tabPanel) {
+
+            var tabPanelList = [];
+
+            // 获取显示的panel
+            $(tabPanel).find('.nav-tab-span').each(function (index, item) {
+                tabPanelList.push($(item).attr('name'));
+            })
+
+            $(tabPanel).find('.nav-tab-span').on('click', function () {
+
+                console.log($(tabPanel).find('.nav-tab-span'))
+
+
+
+                clearClass(tabPanelList);
+                console.log()
+                $('#' + $(this).attr('name')).show();
+
+            })
+
+            // 隐藏不必要
+            clearClass = function (arr) {
+                for (var i in arr) {
+                    $('#' + arr[i]).hide();
+                }
+            }
+
+        }
+
+        for (var i = 0; i < $tabList.length; i++) {
+            (function (d) {
+                changeTab($tabList[d]);
+            })(i)
         }
     }
-    return theRequest;
-}
+    changeTabComponent();
+
+});
--- src/scripts/fabric.js
+++ src/scripts/fabric.js
@@ -7,18 +7,18 @@
  *   1、
  *   2、
  */
-$(function () {
-    to_customization();
-});
+// $(function () {
+//     to_customization();
+// });
 /**
  * 进入文档中心
  */
-function to_customization() {
-    var request = getRequest(),
-        tag = request['tag'];
-    if(tag == 1){//tag为1：从【SaaS定制平台】进入【SaaS面料平台】
-        $('.my-tab').parent().find('.active').removeClass();
-        $('.my-tab').find('a').eq(1).addClass('active');
-        $('.my-tab').find('a').eq(1).click();
-    }
-}
+// function to_customization() {
+//     var request = getRequest(),
+//         tag = request['tag'];
+//     if(tag == 1){//tag为1：从【SaaS定制平台】进入【SaaS面料平台】
+//         $('.my-tab').parent().find('.active').removeClass();
+//         $('.my-tab').find('a').eq(1).addClass('active');
+//         $('.my-tab').find('a').eq(1).click();
+//     }
+// }
